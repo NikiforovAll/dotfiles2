feb3a6ea3407ee36eac8d1a076ac0a8ab8d5887b
Oleksii Nikiforov <oleksii_nikiforov@epam.com>
2026-02-14 09:33:16 +0200

    improve selection

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,8 @@
-oh-my-posh init pwsh --config "~\.nikiforovall.omp.json" | Invoke-Expression
+# Only load oh-my-posh in truly interactive sessions (avoids issues with Copilot CLI subprocesses)
+# Check: not running with -Command/-c, has a console window, and is user interactive
+$cmdArgs = [Environment]::GetCommandLineArgs()
+$isNonInteractive = $cmdArgs -contains '-Command' -or $cmdArgs -contains '-c' -or $cmdArgs -contains '-NonInteractive'
+
+if (-not $isNonInteractive -and $Host.Name -eq 'ConsoleHost' -and [Environment]::UserInteractive) {
+    oh-my-posh init pwsh --config "~\.nikiforovall.omp.json" | Invoke-Expression
+}
